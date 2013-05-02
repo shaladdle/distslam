08b2ff340563dc4366007f185640ebbeeead936a
Tim Kuehn <tkuehn@Cream.(none)>
2013-05-01 22:24:51 -0400

    support for multiple cobots

--- src/main.py
+++ src/main.py
@@ -2,34 +2,48 @@ import numpy as np
 from time import sleep
 from simulator import *
 import graphics as g
+import random
+
+class Cobot:
+    global width, height, lm
+    def __init__(self, P, u=None, x=None):
+        self.P = P
+        if u is None:
+            self.u = np.zeros((2, 1))
+            self.u.fill(1)
+        if x is None:
+            self.x = np.zeros((len(lm) * 2 + 2,1))
+            self.x[0][0] = 200 + random.randrange(-width / 2, width / 2)
+            self.x[1][0] = height
 
 class EstimateDrawer:
     def __init__(self, win):
         self.win = win
         self.points = []
+        self.states = []
 
-        self.x = []
-        self.P = []
+    def draw(self, states):
+        for x, P in self.states:
+            for item in x:
+                item.undraw()
 
-    def draw(self, x, P):
-        x = np.array(x)
-        P = np.array(P)
+            for item in P:
+                item.undraw()
 
-        for item in self.x:
-            item.undraw()
+        self.states = []
 
-        for item in self.P:
-            item.undraw()
+        for x, P in states:
+            x = np.array(x)
+            P = np.array(P)
 
-        self.x = []
-        self.P = []
+            state_x, state_P = [], []
+            for i in range(x.shape[0] // 2):
+                c = g.Circle(g.Point(x[i*2][0], x[i*2+1][0]), 10)
+                c.setOutline("red")
+                c.draw(self.win)
+                state_x.append(c)
+            self.states.append((state_x, state_P))
 
-        for i in range(x.shape[0] / 2):
-            p = g.Circle(g.Point(x[i*2][0], x[i*2+1][0]), 10)
-            p.setOutline("red")
-            p.draw(self.win)
-
-            self.x.append(p)
 
 def kalman_predict(F, G, P, x, u):
     x_p = F * x + G * u
@@ -69,11 +83,11 @@ def getF(x):
     return np.matrix(np.identity((x.shape[0])))
 
 if __name__ == "__main__":
+    global width, height, lm
     width = 400
     height = 400
     win = g.GraphWin('SLAM Simulator', width, height)
 
-    sim = Simulator(win, g.Point(200, 200), width, height)
     lm_points = [ g.Point(30,  40),
                   g.Point(25,  10),
                   g.Point(300, 200),
@@ -88,18 +102,22 @@ if __name__ == "__main__":
 
     print(len(lm))
 
-    sim.set_landmarks(lm)
 
-    x = np.matrix(np.zeros((len(lm) * 2 + 2,1)))
-    P = np.identity(x.shape[0]) * 10
+    # set initial state and covariance matrix
+    numbots = 2
+    P = np.identity(len(lm) * 2 + 2) * 10
     P[0][0] = 100
     P[1][1] = 100
-
     P = np.matrix(P)
+    cobot_sim = []
+    for _ in range(numbots):
+        cobot = Cobot(P)
+        coords = np.array(cobot.x)
+        sim = Simulator(win, g.Point(coords[0][0], coords[1][0]), width, height)
+        sim.set_landmarks(lm)
+        cobot_sim.append((cobot, sim))
 
-    u = np.zeros((2,1))
-    u.fill(1)
-
+    # this shit should be functions, not a class
     ed = EstimateDrawer(win)
 
     while(1):
@@ -107,20 +125,22 @@ if __name__ == "__main__":
         if win.isClosed():
             break
 
-        sim.do_motors(u)
-        z = sim.sense()
+        for cobot, sim in cobot_sim:
+            sim.do_motors(cobot.u)
+            z = sim.sense()
 
-        # Get matrices for this iteration
-        F = getF(x)
-        G = getG(x, u)
-        (x, P) = kalman_predict(F, G, P, x, u)
+            # Get matrices for this iteration
+            F = getF(cobot.x)
+            G = getG(cobot.x, cobot.u)
+            cobot.x, cobot.P = kalman_predict(F, G, cobot.P, cobot.x, cobot.u)
 
-        H = getH(x, z)
-        R = getR(z)
-        (x, P) = kalman_update(H, R, P, x, z)
+            H = getH(cobot.x, z)
+            R = getR(z)
+            cobot.x, cobot.P = kalman_update(H, R, cobot.P, cobot.x, z)
 
-        print(x)
+            print('cobot.x:')
+            print(cobot.x)
 
-        ed.draw(x, P)
+        ed.draw((cobot.x, cobot.P) for cobot, _ in cobot_sim)
 
         sleep(0.5)
